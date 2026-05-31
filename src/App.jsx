67cf25004d248e878ed0f4beaa729d86fbea8e92
Rishabh Tiwari <pt64632@gmail.com>
2026-05-31 17:04:01 +0530

    Checkout page to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes,Route } from 'react-router'
 import {HomePage} from './pages/HomePage.jsx'
-
+import {CheckoutPage} from './pages/CheckoutPage.jsx'
 import './App.css'
 
 function App() {
@@ -9,8 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/orders' element={<h1>Orders Page</h1>} />
-      <Route path='/checkout' element={<h1>Checkout Page</h1>} />
+      <Route path='/checkout' element={<CheckoutPage />} />
     </Routes>
    
   )
